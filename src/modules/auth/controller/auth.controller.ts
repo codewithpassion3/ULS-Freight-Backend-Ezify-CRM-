8c53cca5032fd02f0dde20d12b0cc366ede7f36e
Furqan <furqan@ezaccounts.ca>
2026-03-11 02:27:11 +0500

    feat: logout api

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, HttpCode, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "../service/auth.service";
 import { SignupDTO } from "../dto/signup.dto";
-import type { Request } from "express";
+import type { Request, Response } from "express";
 import { SigninDTO } from "../dto/signin.dto";
+import { SessionAuthGuard } from "src/guards/sessionAuth.guard";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
    constructor(private readonly AuthService: AuthService) {}
 
@@ -28,5 +29,24 @@ export class AuthController {
          message: "Signin successfull",
          user
       }
+   }
+
+   @UseGuards(SessionAuthGuard)
+   @Post('/logout')
+   @HttpCode(200)
+   async logout(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+   return new Promise((resolve, reject) => {
+      req.session.destroy((err) => {
+         if (err) {
+         return reject(err);
+         }
+
+         res.clearCookie("connect.sid");
+
+         resolve({
+         message: "Logged out successfully",
+         });
+      });
+   });
    }
 }
