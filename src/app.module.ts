c7e28261e1fc5fc291ca470e241616c012f53ff3
Furqan <furqan@ezaccounts.ca>
2026-03-10 19:32:40 +0500

    feat: database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import config from './mikro-orm.config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [MikroOrmModule.forRoot({
+    ...config,
+    autoLoadEntities: true
+  }),
+  AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { MikroORM } from '@mikro-orm/postgresql';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true,
+  }));
+
+  app.setGlobalPrefix(`/api/${process.env.API_VERSION || 'v1'}`);
+
+  const orm = app.get(MikroORM);
+  await orm.schema.updateSchema();
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
