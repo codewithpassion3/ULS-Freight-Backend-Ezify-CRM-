ea95d34c981eb4fec9c67711a25b5545d2c1545b
Furqan Anwar <furqan@ezaccounts.ca>
2026-05-21 01:57:02 +0500

    fix: filter out extra permissions for admin role

--- src/modules/permission/service/permission.service.ts
+++ src/modules/permission/service/permission.service.ts
@@ -1,7 +1,7 @@
 import { EntityManager } from "@mikro-orm/core";
 import { Injectable } from "@nestjs/common";
 import { SessionData } from "express-session";
-import { STAFF_ALLOWED_PERMISSIONS } from "src/common/constants/permissions";
+import { ADMIN_EXCLUDED_PERMISSIONS, STAFF_ALLOWED_PERMISSIONS } from "src/common/constants/permissions";
 import { ROLES } from "src/common/constants/roles";
 import { Permission } from "src/entities/permission.entity";
 
@@ -10,9 +10,10 @@ export class PermissionService {
     constructor(private readonly em: EntityManager) {}
 
     async getAll(session: SessionData){
-        console.log({session})
         let permissions = await this.em.findAll(Permission);
 
+        if(session.role === ROLES.ADMIN) return permissions.filter(permission => !ADMIN_EXCLUDED_PERMISSIONS.includes(permission.name))
+        
         if(session.role === ROLES.SUPER_ADMIN) return permissions.filter( permission => STAFF_ALLOWED_PERMISSIONS.includes(permission.name))
         
         return permissions;
